58ad164fc0233263b3a807f70c7f1849ea5e2bd8
mstescha <schaudt@tu-dortmund.de>
2023-02-10 10:29:56 +0100

    Test

--- Programmcodes/Dateneinlesen.py
+++ Programmcodes/Dateneinlesen.py
@@ -6,30 +6,21 @@ from datetime import date, timedelta
 a = 10 #KLänge der Materialnummern
 b = 0 #Anzahl der Ziffern die aus der Materialnummer entfernt werden (Bspw. 7777)
 c = 1 #Wie viele Nachkommastellen bei den Startterminen entfernt werden sollen
-Date = '2022-08-14' #Für den Test hier nur ein beispielhafter Tag
+Date = '2023-01-14' #Für den Test hier nur ein beispielhafter Tag
 Date = pd.to_datetime(Date) #Zeile 9 und 10 können hinterher gelöscht werden und Zeile 11 aktiviert
 #Date = date.today() #Aktueller Tag wird gespeichert
 NextDate = Date + timedelta(days=14) #In den nächsten 14 Tagen wird geschaut, was ansteht
-ST = pd.read_excel('Dateien\Starttermine DOD-4_07.07.2022.xlsx')
-ST = ST[ST['Mat.-Nr.'].notna()] #Sobald eine Zeile in diesem Bereich Leer ist, wird diese gelöscht
-ST['Mat.-Nr.'] = ST['Mat.-Nr.'].str.replace('.' , '')
+
+#Starttermine Produktionsaufträge G20
+Starttermine = pd.read_excel('Dateien\Starttermine G20 - 2023.xlsx')
+Starttermine['Mat.-Nr.'] = Starttermine['Mat.-Nr.'].str.replace('.' , '')
 if b>0:
-    ST['Mat.-Nr.'] = ST['Mat.-Nr.'].str[:-b] #die letzten Ziffern werden entfernt
-ST['Menge'] = ST['Menge'].str.replace(',' , '') #Kommas werden entfernt
-ST['Menge'] = ST['Menge'].str.replace('.' , ',') #Dezimaltrennung ändern
-#ST['Mat.-Nr.'] = ST['Mat.-Nr.'].astype(int) #Materialnummer zu einem Float
-Spliting = ST['Menge'].str.split(expand=True) #Datenbank aufsplitten
-Spliting.rename(columns={0 : 'Menge', 1: 'Einheit'}, inplace=True) #Datenstrukturen Namen geben
-ST.drop(columns='Menge',inplace=True) #Die gesplitteten Spalten aus dem ursprünglichem entfernen
-Start = pd.merge(ST,Spliting,left_index=True,right_index=True) #Datenbanken zusammenfügen
-Start['Menge'] = Start['Menge'].str.replace(',', '') #Hier werden die Kommas aus der Mengeneinheit entfernt
-Start['Menge'] = Start['Menge'].str[:-c] #Hier werden die Nachkommastellen entfernt ACHTUNG: Es wird immmer nur von einer ausgegangen
-Start['Menge'] = Start['Menge'].astype(float) #Datentyp Float erstellen
-Start['Start'] = pd.to_datetime(Start['Start'])
-Start.set_index(['Start'], inplace=True) #Hier wird der Index gesetzt
-Start = Start.sort_values(by='Start') #Hier wird die Liste noch nach den Startdaten geordnet
-Next = Start[Date:NextDate] #Hier werden die Aufträge der nächsten zwei Wochen ausgelesen
-#Next.set_index(['Mat.-Nr.'],inplace=True) #Hier werden die Materialnummern zu einem Index
+    Starttermine['Mat.-Nr.'] = Starttermine['Mat.-Nr.'].str[:-b]
+Starttermine.rename(columns={'Offene Menge':'Menge', 'Beginn (terminiert)':'Start','Ende (terminiert)':'Ende' }, inplace=True)
+Starttermine.set_index(['Start'], inplace=True)
+Starttermine=Starttermine.sort_values(by='Start')
+Next=Starttermine[Date:NextDate]
+print(Next)
 
 #Stücklisten werden eingelesen
 List1 = pd.read_csv('Dateien\STUELI_EL-DOD-4.TXT',names=['Werk',
