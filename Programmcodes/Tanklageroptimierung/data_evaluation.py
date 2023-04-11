7092352eeb6e11b82e9858e693e000af0bbf3d85
mstescha <schaudt@tu-dortmund.de>
2023-04-11 15:50:57 +0200

    Test

--- Programmcodes/Tanklageroptimierung/data_evaluation.py
+++ Programmcodes/Tanklageroptimierung/data_evaluation.py
@@ -10,6 +10,8 @@ class DataEvaluation:
         self.solution = self._read_solution()
         raster_zeitschritte = 8
         self.data = DataReader(raster_zeitschritte)
+        self.rohstoff_mapping = self.data.rohstoff_mapping
+        self.rohstoff_mapping['r'] = self.rohstoff_mapping['r'].astype(str)
         self.e = self._read_e()
         self.x_tilde = self._read_x_tilde()
         self.u = self._read_u()
@@ -33,13 +35,19 @@ class DataEvaluation:
         e = e.type.str.split(pat=" ", expand=True)
         e.rename(columns={0: 'type', 1 : "value"}, inplace=True)
         e[['type', 'Time', 'Material']] = e.type.str.split(pat="_", expand=True)
+        e = pd.merge(e, self.rohstoff_mapping, left_on='Material', right_on='r', how="inner")
+        e.drop(columns=['r'], inplace=True)
         return e
 
     def _read_x_tilde(self) -> pd.DataFrame:
         x_tilde = self.solution[self.solution["type"].str.contains('x_tilde') == True]
         x_tilde = x_tilde.type.str.split(pat=" ", expand=True)
-        #x_tilde.rename(columns={0: 'type', 1 : "value"}, inplace=True)
-        #x_tilde[['type', 'Time', 'Material']] = e.type.str.split(pat="_", expand=True)
+        x_tilde.rename(columns={0: 'reset', 1 : "value"}, inplace=True)
+        x_tilde['type'] = x_tilde['reset'].str[0:7]
+        x_tilde['temp'] = x_tilde['reset'].str[8:]
+        x_tilde[['Time', 'Tank', 'Material']] = x_tilde.temp.str.rsplit(pat="_", expand=True)
+        x_tilde = pd.merge(x_tilde, self.rohstoff_mapping, left_on='Material', right_on='r', how="inner")
+        x_tilde.drop(columns=['reset', 'temp', 'r'], inplace=True)
         return x_tilde
 
     def _read_f(self) -> pd.DataFrame:
@@ -47,6 +55,8 @@ class DataEvaluation:
         f = f.type.str.split(pat=" ", expand=True)
         f.rename(columns={0: 'type', 1 : "value"}, inplace=True)
         f[['type', 'Time','Tank', 'Material']] = f.type.str.split(pat="_", expand=True)
+        f = pd.merge(f, self.rohstoff_mapping, left_on='Material', right_on='r', how="inner")
+        f.drop(columns=['r'], inplace=True)
         return f
 
     def _read_u(self) -> pd.DataFrame:
@@ -54,6 +64,8 @@ class DataEvaluation:
         u = u.type.str.split(pat=" ", expand=True)
         u.rename(columns={0: 'type', 1 : "value"}, inplace=True)
         u[['type', 'Time','Tank', 'Material']] = u.type.str.split(pat="_", expand=True)
+        u = pd.merge(u, self.rohstoff_mapping, left_on='Material', right_on='r', how="inner")
+        u.drop(columns=['r'], inplace=True)
         return u
 
     def _read_v(self) -> pd.DataFrame:
@@ -61,6 +73,8 @@ class DataEvaluation:
         v = v.type.str.split(pat=" ", expand=True)
         v.rename(columns={0: 'type', 1 : "value"}, inplace=True)
         v[['type', 'Time','Tank', 'Material']] = v.type.str.split(pat="_", expand=True)
+        v = pd.merge(v, self.rohstoff_mapping, left_on='Material', right_on='r', how="inner")
+        v.drop(columns=['r'], inplace=True)
         return v
 
     def _read_y(self) -> pd.DataFrame:
@@ -75,6 +89,8 @@ class DataEvaluation:
         l = l.type.str.split(pat=" ", expand=True)
         l.rename(columns={0: 'type', 1 : "value"}, inplace=True)
         l[['type', 'Time', 'Material']] = l.type.str.split(pat="_", expand=True)
+        l = pd.merge(l, self.rohstoff_mapping, left_on='Material', right_on='r', how="inner")
+        l.drop(columns=['r'], inplace=True)
         return l
 
     def _read_s(self) -> pd.DataFrame:
@@ -82,7 +98,6 @@ class DataEvaluation:
         s = s.type.str.split(pat=" ", expand=True)
         s.rename(columns={0: 'type', 1 : "value"}, inplace=True)
         s[['type', 'Time', 'Material']] = s.type.str.split(pat="_", expand=True)
-        #s = self.data.rohstoff_mapping
+        s = pd.merge(s, self.rohstoff_mapping, left_on='Material', right_on='r', how="inner")
+        s.drop(columns=['r'], inplace=True)
         return s
-
-
