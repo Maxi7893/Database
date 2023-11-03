77327b529b20712c2c41912523e97ec2cbd4c7c4
Maxi <Maximilian.Kiefer@tu-dortmund.de>
2023-11-03 17:09:13 +0100

    Patrick Änderungen

--- Programmcodes/Tanklageroptimierung/TestFile.py
+++ Programmcodes/Tanklageroptimierung/TestFile.py
@@ -2,8 +2,6 @@ import csv
 
 import numpy as np
 import pandas as pd
-import seaborn as sns
-from Tanklageroptimierung.data_reader import DataReader
 
 b = pd.read_excel(
     r'C:\Users\mb-itl-sim\Models\Materialflussanalyse_EL-DOD\Database\Programmcodes\Tanklageroptimierung\Auswertung\b_ztr.xlsx', header = 0)
